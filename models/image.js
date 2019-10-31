f274f2e342668e0b28738128bf3a04f57c37af98
Amrit Khera <amritkhera98@gmail.com>
2019-10-31 13:00:48 +0530

    add mongoDB models

---
+++ models/image.js
@@ -0,0 +1,16 @@
+var mongoose  = require("mongoose");
+
+var imageSchema  =  new mongoose.Schema({
+    img_path:String,
+    img_caption:String,
+    img_public:Boolean,
+    author:{
+      id:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+      },
+      username:String
+    }
+});
+
+module.exports=mongoose.model("Image",imageSchema);
